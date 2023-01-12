db3acb100fd5753eb4cb85a729f2058c466dfc0a
Yosafat Kesuma <da005kage@gmail.com>
2023-01-12 16:39:17 +0700

    init react vite vanilla extract

---
+++ src/example/button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { buttonStyle } from "./button.css";
+
+export default function Button() {
+  return (
+    <button type="button" className={buttonStyle}>
+      asdasd
+    </button>
+  );
+}
